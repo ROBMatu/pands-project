bcacc06484f2a7177cbe627ab65f3d4132a10fbc
RobertOBM <G00300191@atu.ie>
2023-04-23 18:57:10 +0100

    plot and readme improvements

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # **pands-project**
 ## Research project on the Fisher’s Iris data set.
-##### Author : Robert O'Brien-Monk
+##### Author : *Robert O'Brien-Monk*
 ---
 ### Synopsis
 
-Fisher's Iris data set, comes from a study of 3 Iris varieties; Iris Setosa, Iris Versicolour, iris Virginica.
+Fisher's Iris data set, comes from a study of 3 Iris varieties; Iris Setosa, Iris Versicolour, Iris Virginica. In a paper written by Ronald Fisher a statistician and biologist in 1936. And has become a common test case for data analysis.
 The dataset consists of the following attributes:
 1. sepal length in cm 
 2. sepal width in cm 
@@ -12,13 +12,11 @@ The dataset consists of the following attributes:
 4. petal width in cm 
 5. class (the variety of Iris)
 
-There are 150 records, consisting of 50 records for each variety of Iris.
-
-This project is an analysis of this dataset.
+There are 150 records in the dataset, consisting of 50 records for each variety of Iris, displaying the measurements of the four attributes. This project is an analysis of this dataset,through the use of python programming to aid in observations of the data and display the data in plots.
 
 ---
 ### analysis.py
-This script carries out my analysis of the data set, where I constructed plots of the attributes of the iris' and I compared the varities to spot any variations between them.
+This script carries out my analysis of the data set, where I constructed plots of the attributes of the iris and I compared the varities to spot any variations between them.
 
 ### Assign variables with attribute labels and read in iris dataset
 I started off by assigning the variables with attribute names in order to format my initial sample analysis of the iris data set.
@@ -27,13 +25,14 @@ I started off by assigning the variables with attribute names in order to format
 
 ### Sample analysis
 
-I used head() to display the first 5 entries of the dataset. Then i used describe() to give me a summary of the entire dataset, with the count, mean, standard variation, minimum value, 25 & 50 & 75 percentiles and max values of the four attributes.
-Then I created some subplots of each of the attributes.
+I used head() to display the first 5 entries of the dataset. Then I used describe() to give me a summary of the entire dataset, with the count, mean, standard variation, minimum value, 25 & 50 & 75 percentiles and max values of the four attributes. To be displayed in the terminal.
+I then ouputted a summary of the data set to a .txt file "iris_data_summary.txt" which gives some statisics of the different Iris attributes.
+Then I created histogram subplots of each of the attributes, with the cm measurements on the x-axis and quantity on the y-axis.
 
 ---
 
 ### Histogram plots of attributes
-I wanted to compare each variety of iris against each other under each of the attributes to see how they differed from eachother.
+I wanted to compare each variety of Iris against each other under each of the attributes to see how they differed from eachother.
 So I created four histogram plots using matplotlib.pyplot as plt. and saved each as a .png file.
 
 ---
@@ -42,20 +41,33 @@ So I created four histogram plots using matplotlib.pyplot as plt. and saved each
 I then created two scatter plots, one to compare the Sepal length against the sepal width, and the other to compare Petal length against Petal Width. And within those scatter plots to compare the different varieties.
 
 ---
+
+### Analysis of Iris dataset
+Iris Setosa had the shortest but widest sepals, and it had the smallest petals by far. Iris Versicolor has the smallest sepal width but is quite close to Iris Virginica, Versicolor is the middle of the range for petal length and width. Iris Virginica has the widest distribution of sepal lengths and is the longest overall, its middle range for sepal width and has the largest petals overall.
+So Setosa has the smallest sepals and petals and Virginica is the largest overall and Versicolour trends quite closely with Virginica.
+
+
+---
 ### **software used and references**
 
  Visual Studio Code - version 1.74.3
+ 
+ matplotlib and pandas modules used
 
  #### Iris data set resource: 
+ 
  [iris.csv](https://archive.ics.uci.edu/ml/datasets/iris)
+
  #### Using pandas:
   [geekforgeeks.org](https://www.geeksforgeeks.org/exploratory-data-analysis-on-iris-dataset/)
 
   [stackoverflow dataframe summary to .txt](https://stackoverflow.com/questions/75196592/how-to-append-the-dataframe-output-to-txt-file)
 
   [stackoverflow dataframe summary to .txt , index='name'](https://stackoverflow.com/questions/32835498/pandas-python-describe-formatting-output)
+
  #### Code reference and to understand project: 
- [angela1C github](https://github.com/angela1C/Programming-and-Scripting-Project-2019/blob/master/project_iris.py)
+ [angela1C github, for histogram subplots](https://github.com/angela1C/Programming-and-Scripting-Project-2019/blob/master/project_iris.py)
+
 #### python matplotlib histograms & pandas: 
 [nbshare.io](https://www.nbshare.io/notebook/204214467/How-to-Plot-a-Histogram-in-Python/)
 
@@ -68,3 +80,8 @@ I then created two scatter plots, one to compare the Sepal length against the se
 [matplotlib.org colours](https://matplotlib.org/stable/gallery/color/named_colors.html)
 
 [matplotlib bins](https://stackoverflow.com/questions/33458566/how-to-choose-bins-in-matplotlib-histogram)
+
+[stackoverflow plt.close()](https://stackoverflow.com/questions/61551562/how-to-avoid-2-plots-being-wrongly-mixed)
+
+#### other sources
+[wikipedia general Iris data set info ](https://en.wikipedia.org/wiki/Iris_flower_data_set)
--- analysis.py
+++ analysis.py
@@ -3,10 +3,9 @@
 # Outputs a summary of each variable to a single text file,
 # Saves a histogram of each variable to png files,
 # Outputs a scatter plot of each pair of variables.
-# Performs any other analysis
+# 
 
 import pandas
-import numpy as np
 import matplotlib.pyplot as plt
 
 # assign variables with attribute labels and read in iris dataset
@@ -14,11 +13,12 @@ iris_data_attributes = ['Sepal length','Sepal width','Petal length','Petal width
 iris_read = pandas.read_csv("iris.csv",names = iris_data_attributes)
 
 # head() displays first 5 rows
-print("Here is an example of the dataset")
+print("An example of the dataset")
 print(iris_read.head())
+print("")
 
 # describe() showing a statistical summary of the entire dataset
-print("Here is a statistical summary of the dataset")
+print("A statistical summary of the dataset")
 print(iris_read.describe())
 
 # Data summary to txt file
@@ -30,85 +30,91 @@ with open("iris_data_summary.txt","w")as f:
     f.write(iris_summary.to_string(index='name'))
 
 
-# histogram subplots of each of the iris attributes 
-iris_read.hist(alpha=0.8, bins=50, figsize=(12,8))
+# histogram subplots of each of the iris attributes  
+iris_read.hist(bins=30, figsize=(12,8), grid=False)
+plt.suptitle("Iris dataset attributes")
+plt.savefig("iris_attributes.png")
 #plt.show()
-#plt.savefig("iris_attributes.png")
+plt.close()
 
 
 # Font style used in plots
-font= {'family':'Times','color':'purple', 'size':20}
+font = {'family':'Times','color':'purple', 'size':20}
 subfont = {'family':'Times','color':'green', 'size':15}
 
-# Sepal length histogram comparing varieties
+# Sepal length histogram comparing varieties of Iris
 
-sl_setosa= iris_read.iloc[:50,[0]]
-sl_versicolor= iris_read.iloc[51:101,[0]]
-sl_virginica= iris_read.iloc[101:151,[0]]
+sl_setosa = iris_read.iloc[:50,[0]]
+sl_versicolor = iris_read.iloc[51:101,[0]]
+sl_virginica = iris_read.iloc[101:151,[0]]
 
-plt.hist(sl_setosa, bins=25,label='setosa',color='violet')
-plt.hist(sl_versicolor, bins=25,label='versicolor',color='yellow')
-plt.hist(sl_virginica, bins=25,label='virginica',color='purple')
-# graph styling
+plt.hist(sl_setosa, bins=15,label='setosa',color='violet')
+plt.hist(sl_versicolor, bins=15,label='versicolor',color='yellow')
+plt.hist(sl_virginica, bins=20,label='virginica',color='purple')
+# Further graph styling
 plt.title("Sepal length distribution of Iris varieties", fontdict=font)
 plt.xlabel("cm", fontdict=subfont)
 plt.ylabel("Quantity", fontdict=subfont)
 plt.legend(loc='upper right')
+plt.savefig("sepal_length.png")
 #plt.show()
-#plt.savefig("sepal_length.png")
+plt.close()
 
+# Sepal Width histogram comparing varieties of Iris
 
-# Sepal Width histogram comparing varieties
-sw_setosa= iris_read.iloc[:50,[1]]
-sw_versicolor= iris_read.iloc[51:101,[1]]
-sw_virginica= iris_read.iloc[101:151,[1]]
+sw_setosa = iris_read.iloc[:50,[1]]
+sw_versicolor = iris_read.iloc[51:101,[1]]
+sw_virginica = iris_read.iloc[101:151,[1]]
 
-plt.hist(sw_setosa, bins=25,label='setosa',color='violet')
-plt.hist(sw_versicolor, bins=25,label='versicolor',color='yellow')
-plt.hist(sw_virginica, bins=25,label='virginica',color='purple')
+plt.hist(sw_setosa, bins=20,label='setosa',color='violet')
+plt.hist(sw_versicolor, bins=20,label='versicolor',color='yellow')
+plt.hist(sw_virginica, bins=20,label='virginica',color='purple')
 # graph styling
 plt.title("Sepal width distribution of Iris varieties", fontdict=font)
 plt.xlabel("cm", fontdict=subfont)
 plt.ylabel("Quantity", fontdict=subfont)
 plt.legend(loc='upper right')
+plt.savefig("sepal_width.png")
 #plt.show()
-#plt.savefig("sepal_width.png")
+plt.close()
 
+# Petal length histogram comparing varieties  of Iris
 
-# Petal length histogram comparing varieties
-pl_setosa= iris_read.iloc[:50,[2]]
-pl_versicolor= iris_read.iloc[51:101,[2]]
-pl_virginica= iris_read.iloc[101:151,[2]]
+pl_setosa = iris_read.iloc[:50,[2]]
+pl_versicolor = iris_read.iloc[51:101,[2]]
+pl_virginica = iris_read.iloc[101:151,[2]]
 
-plt.hist(pl_setosa, bins=25,label='setosa',color='violet')
-plt.hist(pl_versicolor, bins=25,label='versicolor',color='yellow')
-plt.hist(pl_virginica, bins=25,label='virginica',color='purple')
+plt.hist(pl_setosa, bins=10,label='setosa',color='violet')
+plt.hist(pl_versicolor, bins=15,label='versicolor',color='yellow')
+plt.hist(pl_virginica, bins=15,label='virginica',color='purple')
 # graph styling
 plt.title("Petal length distribution of Iris varieties", fontdict=font)
 plt.xlabel("cm", fontdict=subfont)
 plt.ylabel("Quantity", fontdict=subfont)
 plt.legend(loc='upper right')
+plt.savefig("petal_length.png")
 #plt.show()
-#plt.savefig("petal_length.png")
+plt.close()
 
+# petal Width histogram comparing varieties  of Iris
 
-# petal Width histogram comparing varieties
-pw_setosa= iris_read.iloc[:50,[3]]
-pw_versicolor= iris_read.iloc[51:101,[3]]
-pw_virginica= iris_read.iloc[101:151,[3]]
+pw_setosa = iris_read.iloc[:50,[3]]
+pw_versicolor = iris_read.iloc[51:101,[3]]
+pw_virginica = iris_read.iloc[101:151,[3]]
 
-plt.hist(pw_setosa, bins=25,label='setosa',color='violet')
-plt.hist(pw_versicolor, bins=25,label='versicolor',color='yellow')
-plt.hist(pw_virginica, bins=25,label='virginica',color='purple')
+plt.hist(pw_setosa, bins=10,label='setosa',color='violet')
+plt.hist(pw_versicolor, bins=10,label='versicolor',color='yellow')
+plt.hist(pw_virginica, bins=10,label='virginica',color='purple')
 # graph styling
 plt.title("Petal width distribution of Iris varieties", fontdict=font)
 plt.xlabel("cm", fontdict=subfont)
 plt.ylabel("Quantity", fontdict=subfont)
 plt.legend(loc='upper right')
+plt.savefig("petal_width.png")
 #plt.show()
-#plt.savefig("petal_width.png")
+plt.close()
 
-# Sepal length vs Sepal width scatter plot
+# Sepal length vs Sepal width scatter plot, comparing varieties  of Iris
 
 plt.scatter(sl_setosa,sw_setosa,label='setosa', color='violet')
 plt.scatter(sl_versicolor,sw_versicolor,label='versicolor', color='yellow')
@@ -116,16 +122,20 @@ plt.scatter(sl_virginica,sw_virginica,label='virginica', color='purple')
 plt.title("Sepal length vs Sepal width",fontdict=font)
 plt.xlabel("Sepal length cm",fontdict=subfont)
 plt.ylabel("Sepal width cm",fontdict=subfont)
+plt.legend(loc='upper right')
+plt.savefig("sepal_length_vs_sepal_width.png")
 #plt.show()
-#plt.savefig("sepal_length_vs_sepal_width.png")
+plt.close()
 
+# Petal Length vs Petal width scatter plot, comparing varieties  of Iris
 
-# Petal Length vs Petal width
 plt.scatter(pl_setosa,pw_setosa,label='setosa', color='violet')
 plt.scatter(pl_versicolor,pw_versicolor,label='versicolor', color='yellow')
 plt.scatter(pl_virginica,pw_virginica,label='virginica', color='purple')
 plt.title("Petal length vs Petal width",fontdict=font)
 plt.xlabel("Petal length cm",fontdict=subfont)
 plt.ylabel("Petal width cm",fontdict=subfont)
+plt.legend(loc='upper left')
+plt.savefig("petal_length_vs_petal_width.png")
 #plt.show()
-#plt.savefig("petal_length_vs_petal_width.png")
+plt.close()
